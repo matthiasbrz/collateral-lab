2b95403754fe2ff7a2d22e8868da334ab09e6e30
Matthias Brzustowski <91619829+matthiasbrz@users.noreply.github.com>
2026-09-21 14:16:51 +0200

    refactor: suppression de la chaine Python de transformation

--- sql/07_mart_prix_m2_reference.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE TABLE mart_prix_m2_reference AS
-SELECT code_commune, nom_commune, type_local, mois,
-        prix_m2_median_12m, prix_m2_q1_12m, prix_m2_q3_12m,
-        nb_mutations_12m, evolution_pct, ecart_interquartile, dispersion_relative
-FROM agg_prix_m2_evolution
-LEFT JOIN dim_commune USING (code_commune)
-WHERE fenetre_complete
-    AND nb_mutations_12m >=5; -- seuil justifie dans docs/gouvernance.md
--- sql/08_ref_seuils_dispersion.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE TABLE ref_seuils_dispersion AS
-SELECT type_local,
-       round(quantile_cont(dispersion_relative, 0.33), 3) AS borne_basse,
-       round(quantile_cont(dispersion_relative, 0.67), 3) AS borne_haute,
-       round(quantile_cont(dispersion_relative, 0.50), 3) AS mediane
-FROM mart_prix_m2_reference
-GROUP BY type_local
